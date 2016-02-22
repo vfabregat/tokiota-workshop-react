3f744f1634cd2ec4f5f16cee3f6533c2c53a6057
Marc Fuentes Montenegro <marc.fuentes@tokiota.com>
2016-02-22 16:52:20 +0100

    Chat reset

--- src/js/components/chat.js
+++ src/js/components/chat.js
@@ -4,14 +4,9 @@ var Textbox = require('./textbox');
 var Login = require('./login');
 
 var Chat = React.createClass({
-    
-    getDefaultProps: function(){},
+    render: function(){
 
-    // Invoked once before the component is mounted, sets the initial state
-    getInitialState: function(){},
-
-    // virtual representation of a native DOM component, does not modify component state
-    render: function(){}
+    }
 });
 
 module.exports = Chat;
