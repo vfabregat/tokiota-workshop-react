c4a19c8263193032cfb7e537121482474b23224f
Marc Fuentes Montenegro <marc.fuentes@tokiota.com>
2016-02-22 21:56:32 +0100

    Fixes

--- src/js/components/chat.js
+++ src/js/components/chat.js
@@ -5,7 +5,7 @@ var Login = require('./login');
 
 var Chat = React.createClass({
     render: function(){
-
+        return null;
     }
 });
 
--- src/js/services/services.js
+++ src/js/services/services.js
@@ -1,7 +1,5 @@
 var login = require('./login');
-var message = require('./message');
 
 module.exports = {
-    login: login,
-    message: message
+    login: login
 };
